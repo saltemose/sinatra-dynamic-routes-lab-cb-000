de481e64db30d3abd966570020b4d4b82c6586ef
Sean Altemose <sean@seanaltemose.com>
2018-06-26 22:10:33 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -27,5 +27,10 @@ class App < Sinatra::Base
     "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
 
+  get '/:operation/:number1/:number2' do
+    op = params[:operation]
+    num1 = params[:number1]
+    num2 = params[:number2]
+    num1 + op + num2 
 
 end
